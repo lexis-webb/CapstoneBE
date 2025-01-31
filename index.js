3b91a482fafc251fb3e68cbf1d3ba53c6eb69664
lexis-webb <lexiswebb.design@gmail.com>
2025-01-30 21:43:07 -0500

    added Schema

--- index.js
+++ index.js
@@ -36,6 +36,40 @@ app.post("/upload", upload.single('product'), (req,res) => {
         image_url: `http://localhost:${PORT}/images/${req.file.filename}`
     })
 })
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
