f54c91debcbe15c284e7f884368ed9f507d8037d
lexis-webb <lexiswebb.design@gmail.com>
2025-02-02 20:55:23 -0500

    added reservation schemas

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const PORT = 4000;
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import reservationRouter from "./routes/reservationRoute.js";
+
 
 
 const app = express();
@@ -18,6 +20,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
+app.use("api/v1/reservations", reservationRouter)
 
 
 
