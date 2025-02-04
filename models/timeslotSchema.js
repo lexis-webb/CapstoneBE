80edf58f7c1ca7a4e55847b634804a1122f22e81
lexis-webb <lexiswebb.design@gmail.com>
2025-02-03 22:17:21 -0500

    updated schema

--- models/timeslotSchema.js
+++ models/timeslotSchema.js
@@ -2,26 +2,34 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const timeSlotSchema = new mongoose.Schema(
-  {
-    date: {
-        type: Date,
-        required: true
-    },
-    slot: {
-        type: String,
-        required: true
-    },
-    people: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate: [validator.isEmail, "Provide a valid email"],
-        }
-},
-    {timestamps : true}
-); 
+    {
+        date: {
+          type: Date,
+          required: true,
+        },
+        slot: {
+          type: String,
+          required: true,
+        },
+        people: {
+          type: Number,
+          required: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          validate: [validator.isEmail, "Provide a valid email"],
+        },
+        status: {
+          type: String,
+          enum: ["available", "booked", "pending"], 
+        },
+        notes: {
+          type: String,
+          trim: true, 
+        },
+      },
+      { timestamps: true }
+    );
 
 export default mongoose.model("TimeSlot", timeSlotSchema);
